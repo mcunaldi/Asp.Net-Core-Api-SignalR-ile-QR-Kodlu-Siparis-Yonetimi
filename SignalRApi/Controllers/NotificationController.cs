21ec89202d780aa9522ea002a96d6d1bad7f4473
Mehmet Can ÜNALDI <mehmetcanunaldi@gmail.com>
2024-01-09 01:41:07 +0300

    add notificationsController

--- SignalRApi/Controllers/NotificationController.cs
+++ SignalRApi/Controllers/NotificationController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SignalR.BusinessLayer.Abstract;
+using SignalR.DtoLayer.NotificationDto;
+using SignalR.EntityLayer.Entities;
+using SignalRApi.Controllers.DAL.Entities;
 
 namespace SignalRApi.Controllers;
 [Route("api/[controller]/[action]")]
@@ -32,5 +35,55 @@ public class NotificationController : ControllerBase
     public IActionResult GetAllNotificationByFalse()
     {
         return Ok(_notificationService.TGetAllNotificationByFalse());
+    }
+
+    [HttpPost]
+
+    public IActionResult CreateNotification(CreateNotificationDto createNotificationDto)
+    {
+        Notification notification = new Notification()
+        {
+            Description = createNotificationDto.Description,
+            Icon = createNotificationDto.Icon,
+            Status = false,
+            Type = createNotificationDto.Type,
+            Date = Convert.ToDateTime(DateTime.Now.ToShortDateString())
+        };
+
+        _notificationService.TAdd(notification);
+        return Ok("Ekleme işlemi başarıyla yapıldı.");
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteNotification(int id)
+    {
+        var value = _notificationService.TGetById(id);
+        _notificationService.TDelete(value);
+        return Ok("Bildirim silindi.");
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult GetNotification(int id)
+    {
+        var value = _notificationService.TGetById(id);
+        return Ok(value);
+    }
+
+    [HttpPut]
+
+    public IActionResult UpdateNotification(UpdateNotificationDto updateNotificationDto)
+    {
+        Notification notification = new Notification()
+        {
+            NotificationId = updateNotificationDto.NotificationId,
+            Description = updateNotificationDto.Description,
+            Icon = updateNotificationDto.Icon,
+            Status = updateNotificationDto.Status,
+            Type = updateNotificationDto.Type,
+            Date = updateNotificationDto.Date
+        };
+
+        _notificationService.TUpdate(notification);
+        return Ok("Güncelleme işlemi başarıyla yapıldı.");
     }
 }
